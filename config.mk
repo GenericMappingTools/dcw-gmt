ea709b4548c91a3e6d1bfbcaab77b5641bf27a05
Paul Wessel <pwessel@hawaii.edu>
2020-07-22 08:25:42 -1000

    Call it 2.0.0

--- config.mk
+++ config.mk
@@ -7,10 +7,10 @@
 
 # DCW data version to be released:
 GMT_VERSION	= 6.1.1
-DCW_VERSION	= 1.2.0
+DCW_VERSION	= 2.0.0
 DCW_DATE	= 2020-AUG-01
 DCW_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/dcw
 GMT_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/gmt
 DCW_WEBSITE	= imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/pwessel/dcw
 # Point to GNU version of tar:
-GNUTAR            = $(shell which gnutar || which gtar || which tar)
+GNUTAR      = $(shell which gnutar || which gtar || which tar)
