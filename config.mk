a4c35c2f19e0ed9bf5d571aa8de3998eb29826fc
Paul Wessel <pwessel@hawaii.edu>
2020-07-21 19:38:37 -1000

    Update Italy with San Marino and Vatican State holes (#5)
    * Update Italy with San Marino and Vatican State holes
    
    Also encode segment headers with lon = 65535 and lat = 0 or 1, with 1 meaning a hole polygons.  These only apply to ZA and IT and appear at the end of the polygon list.
    
    * Update version for next release
    
    * Update DCW2singleshortnc.sh
    
    * Update DCW2singleshortnc.sh
    
    * Finalize 1.1.5

--- config.mk
+++ config.mk
@@ -3,11 +3,11 @@
 #
 # These are the default settings.  You can override them by supplying your
 # own config.mk file which will take precedence.
-# Paul Wessel, July 2018
+# Paul Wessel, July 2020
 
 # DCW data version to be released:
-DCW_VERSION	= 1.1.4
-DCW_DATE	= 2018-JUL-01
+DCW_VERSION	= 1.1.5
+DCW_DATE	= 2020-AUG-01
 DCW_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/dcw
 GMT_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/gmt
 DCW_WEBSITE	= imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/pwessel/dcw
