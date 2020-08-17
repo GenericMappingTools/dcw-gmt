528639e9a253f2a975983a36583388608dd75045
Paul Wessel <pwessel@hawaii.edu>
2020-08-16 16:25:52 -1000

    Adjust dcw-figure.sh to handle CPT column of LB (#12)

--- dcw-figure.sh
+++ dcw-figure.sh
@@ -9,7 +9,7 @@ gmt makecpt -T0/$n_poly/1 -Ccategorical -N > /tmp/$$.cpt
 paste /tmp/$$.cpt /tmp/$$.C.txt > /tmp/$$.txt
 grep -v '^#' dcw-states.txt | awk '{printf "%s.%s\n", $1, $2}' > /tmp/$$.S.txt
 gmt psxy -Rg -JN0/9i -Xc -K -T > $ps
-while read z color continent country name; do
+while read z color flag	continent country name; do
 	echo "Adding ${country} $name"
 	gmt pscoast -R -J -O -K -E${country}+g${color}+pfaint --DIR_DCW=$here >> $ps
 done < /tmp/$$.txt
