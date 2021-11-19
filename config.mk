ba907f30e4938d6c229e034e90df739d2d2ffb96
Paul Wessel <pwessel@hawaii.edu>
2021-11-18 16:50:20 -1000

    Update config.mk

--- config.mk
+++ config.mk
@@ -3,12 +3,12 @@
 #
 # These are the default settings.  You can override them by supplying your
 # own config.mk file which will take precedence.
-# Paul Wessel, April 2021
+# Paul Wessel, Nov 2021
 
 # DCW data version to be released:
 GMT_VERSION	= 6.3.0
 DCW_VERSION	= 2.0.1
-DCW_DATE	= 2021-SEP-09
+DCW_DATE	= 2021-NOV-18
 DCW_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/dcw
 GMT_FTPSITE	= ftp.soest.hawaii.edu:/export/ftp1/ftp/pub/gmt
 DCW_WEBSITE	= imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/pwessel/dcw
